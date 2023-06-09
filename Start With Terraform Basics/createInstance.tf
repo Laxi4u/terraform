df17bb6641522d8f6f54361dd67e0234f8cb2e8f
Laxi4u <85995291+Laxi4u@users.noreply.github.com>
2023-06-09 10:44:09 +0530

    Update createInstance.tf

--- Start With Terraform Basics/createInstance.tf
+++ Start With Terraform Basics/createInstance.tf
@@ -1,8 +1,8 @@
 
 resource "aws_instance" "suresh" {
   count         = 3
-  ami           = "ami-0bff25b43a4479334"
-  instance_type = "t4g.micro"
+  ami           = "ami-0e7e9cf689200b454"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "demoinstnce-${count.index}"
